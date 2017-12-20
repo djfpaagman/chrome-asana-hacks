803aa42ac81775cfc165cfd4639ecd224ceb90e7
Dennis Paagman <dennispaagman@gmail.com>
2017-12-20 11:11:23 +0100

    Rewrite code to use actual value of the checkbox without additional checks

--- settings.js
+++ settings.js
@@ -5,38 +5,22 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('meeting-mode-value').checked = data.meeting_mode;
   })
 
-  document.getElementById('inbox').addEventListener('click', function() {
-    chrome.storage.sync.get({ inbox: false }, function(data) {
-      if (data.inbox) {
-        chrome.storage.sync.set({inbox: false});
-        document.getElementById('inbox-value').checked = false;
-      } else {
-        chrome.storage.sync.set({inbox: true});
-        document.getElementById('inbox-value').checked = true;
-      }
+  document.getElementById('inbox-value').addEventListener('change', function() {
+    chrome.storage.sync.set({ inbox: this.checked });
 
-      chrome.tabs.query({ url: 'https://app.asana.com/*' }, function(tabs) {
-        tabs.forEach(function(tab) {
-          chrome.tabs.sendMessage(tab.id, { toggle: "inbox" });
-        });
+    chrome.tabs.query({ url: 'https://app.asana.com/*' }, function(tabs) {
+      tabs.forEach(function(tab) {
+        chrome.tabs.sendMessage(tab.id, { toggle: "inbox" });
       });
     });
   });
 
-  document.getElementById('meeting-mode').addEventListener('click', function() {
-    chrome.storage.sync.get({ meeting_mode: false }, function(data) {
-      if (data.meeting_mode) {
-        chrome.storage.sync.set({meeting_mode: false});
-        document.getElementById('meeting-mode-value').checked = false;
-      } else {
-        chrome.storage.sync.set({meeting_mode: true});
-        document.getElementById('meeting-mode-value').checked = true;
-      }
+  document.getElementById('meeting-mode-value').addEventListener('change', function() {
+    chrome.storage.sync.set({ meeting_mode: this.checked });
 
-      chrome.tabs.query({ url: 'https://app.asana.com/*' }, function(tabs) {
-        tabs.forEach(function(tab) {
-          chrome.tabs.sendMessage(tab.id, { toggle: "meeting-mode" });
-        });
+    chrome.tabs.query({ url: 'https://app.asana.com/*' }, function(tabs) {
+      tabs.forEach(function(tab) {
+        chrome.tabs.sendMessage(tab.id, { toggle: "meeting-mode" });
       });
     });
   });
