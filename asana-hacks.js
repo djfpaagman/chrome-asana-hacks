1d284317215abdc7f6c97f125184dd3020b9111c
Dennis Paagman <dennispaagman@gmail.com>
2018-01-03 17:14:58 +0100

    Add 'silent mode' setting to make hiding explicit

--- asana-hacks.js
+++ asana-hacks.js
@@ -2,17 +2,30 @@
   function() {
     'use strict';
 
-    function hideNotificationStuff() {
+    var originalTitle;
+
+    function hideNotificationBlobInTitle() {
       if (document.title[0] === "●") {
+        originalTitle = document.title;
+
         document.title = document.title.substr(2, document.title.length);
       }
     }
 
+    // creates an observer to monitor changes in the <title>
+    var titleObserver = new MutationObserver(
+      function(mutations) {
+        hideNotificationBlobInTitle();
+      }
+    );
+
     function toggleInbox() {
       chrome.storage.sync.get({ inbox: true }, function(data) {
         if (data.inbox) {
+          // hide the Inbox button
          document.querySelector('.Topbar-notificationsButton').style.display = '';
         } else {
+          // show the Inbox button
          document.querySelector('.Topbar-notificationsButton').style.display = 'none';
         }
       });
@@ -21,10 +34,12 @@
     function toggleMeetingMode() {
       chrome.storage.sync.get({ meeting_mode: false }, function(data) {
         if (data.meeting_mode) {
+          // hide all tags and project labels
          document.querySelectorAll('.PotPillsContainer').forEach(function(el) {
            el.style.display = 'none';
          });
         } else {
+          // show all tags and project labels
          document.querySelectorAll('.PotPillsContainer').forEach(function(el) {
            el.style.display = '';
          });
@@ -32,20 +47,34 @@
       });
     }
 
-    // create an observer instance
-    var titleObserver = new MutationObserver(
-      function(mutations) {
-        hideNotificationStuff();
-      }
-    );
+    function toggleSilentMode() {
+      chrome.storage.sync.get({ silent_mode: false }, function(data) {
+        if (data.silent_mode) {
+          // Observe changes in the <title> and hide the blob when it changes
+          titleObserver.observe(
+            document.querySelector('title'),
+            { attributes: true, childList: true, characterData: true }
+          );
 
-    // pass in the target node, as well as the observer options
-    titleObserver.observe(
-      document.querySelector('title'),
-      { attributes: true, childList: true, characterData: true }
-    );
+          // Also remove it if already present
+          hideNotificationBlobInTitle();
 
-    // Listen for messages to toggle the inbox
+          // And hide the blob behind "Inbox"
+          document.querySelector('.has-newNotifications').classList.add("asana-hacks-hide");
+        } else {
+          // Stop observing changes to the <title>
+          titleObserver.disconnect();
+
+          // Put back original <title>
+          document.title = originalTitle;
+
+          // Show blob behind "inbox"
+          document.querySelector('.has-newNotifications').classList.remove("asana-hacks-hide");
+        }
+      });
+    }
+
+    // Listen for messages to toggle features, sent by the settings panel
     chrome.runtime.onMessage.addListener(
       function(request, sender, sendResponse) {
         switch(request.toggle) {
@@ -55,17 +84,20 @@
           case "meeting-mode":
             toggleMeetingMode();
             break;
+          case "silent-mode":
+            toggleSilentMode();
+            break;
         }
       }
     );
 
-    // Wait a second before hiding stuff, because stuff is not guaranteed to
+    // Wait a second before toggling everything, because stuff is not guaranteed to
     // have been loaded yet. "run_at": "document_idle" is not always late enough,
     // probably because stuff is still loaded asynchroniously.
     setTimeout(function() {
       toggleInbox();
       toggleMeetingMode();
-      hideNotificationStuff();
+      toggleSilentMode();
     }, 1000);
   }
 )();
--- settings.js
+++ settings.js
@@ -1,9 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
   // Load config settings
-  chrome.storage.sync.get({ inbox: true, meeting_mode: false }, function(data) {
-    document.getElementById('inbox-value').checked = data.inbox;
-    document.getElementById('meeting-mode-value').checked = data.meeting_mode;
-  })
+  chrome.storage.sync.get(
+    { inbox: true, meeting_mode: false, silent_mode: true },
+    function(data) {
+      document.getElementById('inbox-value').checked = data.inbox;
+      document.getElementById('meeting-mode-value').checked = data.meeting_mode;
+      document.getElementById('silent-mode-value').checked = data.silent_mode;
+    }
+  )
 
   document.getElementById('inbox-value').addEventListener('change', function() {
     chrome.storage.sync.set({ inbox: this.checked });
@@ -21,6 +25,16 @@ document.addEventListener('DOMContentLoaded', function() {
     chrome.tabs.query({ url: 'https://app.asana.com/*' }, function(tabs) {
       tabs.forEach(function(tab) {
         chrome.tabs.sendMessage(tab.id, { toggle: "meeting-mode" });
+      });
+    });
+  });
+
+  document.getElementById('silent-mode-value').addEventListener('change', function() {
+    chrome.storage.sync.set({ silent_mode: this.checked });
+
+    chrome.tabs.query({ url: 'https://app.asana.com/*' }, function(tabs) {
+      tabs.forEach(function(tab) {
+        chrome.tabs.sendMessage(tab.id, { toggle: "silent-mode" });
       });
     });
   });
